bf3673a6ea4d8c1b5ae2491872b1ae1117145207
mariacalderonp <mafecp792@gmail.com>
2026-06-29 00:02:04 +0000

    Pregunta 8 de la semana 6 terminada

--- semana6/server.js
+++ semana6/server.js
@@ -14,8 +14,18 @@ app.post('/registro', (req, res) => {
     mensaje: mensaje
   });
 
-});
+}); 
+// Ruta POST para registrar incidencias comunitarias (Pregunta 8)
+app.post('/incidencia', (req, res) => {
+    const tipo = req.body.tipo;
+    const descripcion = req.body.descripcion;
 
+    res.json({
+        mensaje: "Incidencia registrada",
+        tipo: tipo,
+        descripcion: descripcion
+    });
+});
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
